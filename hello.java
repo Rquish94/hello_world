2490eae530f0405dc228e10a09b8d43dd97f207b
Rquish94 <rquish94@live.com>
2017-10-25 08:13:51 -0400

    updated file
    I added the specifications listed for this assignment.
    easier in browser.

--- hello.java
+++ hello.java
@@ -1,18 +1,27 @@
-// a simple program to practice common Github commands 
+
+// a simple program to practice common Github commands
+import java.util.Scanner;
 
 public class hello {
 
     public static void main(String [] args) {
 
+		Scanner s = new Scanner ( System.in );
 
+<<<<<<< HEAD
+        //i edited this page. yea!
+=======
 	
 	// added  line	
 	// added another line
+>>>>>>> cad278aae191a12e1647d2cfa58770da6b62fc76
 
         System.out.println("Hello Class!");
+        String input;
+
+        input = s.nextLine();
 
 
-
+      System.out.println("hello "+ input + "My name is Ryan");
     }
 }
-
